9b74208b9b3441593538fd1cac76068c3325cb71
Erik Botö <erik.boto@pelagicore.com>
2017-12-06 18:18:20 +0100

    buttonreader: should work now

--- buttonreader/buttonreader.py
+++ buttonreader/buttonreader.py
@@ -6,8 +6,11 @@ import RPi.GPIO as GPIO
 from dbus.mainloop.glib import DBusGMainLoop
 
 
-def button_pressed(channel):
-    print monarkControl.targetPower()
+def left_button_pressed(channel):
+    monarkControl.incGear()
+
+def right_button_pressed(channel):
+    monarkControl.decGear()
 
 DBusGMainLoop(set_as_default=True)
 
@@ -23,7 +26,10 @@ ml = gobject.MainLoop()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(26, GPIO.FALLING, callback=button_pressed, bouncetime=100)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(4, GPIO.FALLING, callback=left_button_pressed, bouncetime=100)
+GPIO.add_event_detect(26, GPIO.FALLING, callback=right_button_pressed, bouncetime=100)
 
+# gobject.timeout_add(1000, helloer)
 gobject.threads_init()
 ml.run()
