44065835f641fc2e2e9507bbaa75d14fd02ea5f6
Erik Botö <erik.boto@gmail.com>
2020-05-18 10:16:12 +0200

    fake_monark.py: update to python3

--- test/fake_monark.py
+++ test/fake_monark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import serial
 import time
@@ -6,25 +6,28 @@ import random
 
 s = serial.Serial('/tmp/monark-test2', 4800, timeout=0)
 
+
+
 while True:
-    foo = str(s.read(100))
+    foo = s.read(100).decode('utf-8')
     foo = foo.replace("\r","")
     if len(foo) > 0:
-        print "Got cmd:", foo
+        print("Got cmd:", foo)
+        print(repr(foo))
 
     if foo.startswith("id"):
-        s.write("novofake\r")
+        s.write("novofake\r".encode('utf-8'))
     if foo.startswith("pedal"):
         val = int(90 + random.randint(-5,5))
-        s.write("%s\r" % val)
-        print "Sent:", val
+        s.write(("%s\r" % val).encode('utf-8'))
+        print ("Sent:", val)
     if foo.startswith("pulse"):
         val = int(150 + random.randint(-5,5))
-        s.write("%s\r" % val)
-        print "Sent:", val
+        s.write(("%s\r" % val).encode('utf-8'))
+        print("Sent:", val)
     if foo.startswith("power"):
         val = int(250 + random.randint(-10,10))
-        s.write("%s\r" % val)
-        print "Sent:", val
+        s.write(("%s\r" % val).encode('utf-8'))
+        print("Sent:", val)
     time.sleep(0.01)
     #if foo.beginswith("id"):
